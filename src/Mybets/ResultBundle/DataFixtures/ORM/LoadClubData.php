04027155a549a62ac07140d0b3dd512709e577c6
Janne Vuori <janne.vuori@gmail.com>
2013-04-03 06:36:24 +0300

    Fixtures fixed

--- src/Mybets/ResultBundle/DataFixtures/ORM/LoadClubData.php
+++ src/Mybets/ResultBundle/DataFixtures/ORM/LoadClubData.php
@@ -14,7 +14,7 @@ class LoadClubData extends AbstractFixture implements OrderedFixtureInterface
     {
         $clubs = array("club-tottenham"      => array('name' => "Tottenham Hotspur Football Club", 'city' => "London"),
                        "club-arsenal"       => array('name' => "Arsenal Football Club", 'city' => "London"),
-                       "club-fcbarcelona"   => array('name' => "Futbol Club Barcelona", 'city' => "London"),
+                       "club-fcbarcelona"   => array('name' => "Futbol Club Barcelona", 'city' => "Barcelona"),
                        "club-realmadrid"    => array('name' => "Real Madrid Club de Fútbol", 'city' => "Madrid"));
         foreach($clubs as $ref => $s)
         {
